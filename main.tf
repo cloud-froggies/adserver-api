765c0ab8479047ace23ab1415a5ab5f2b4fa9e4f
Juan Elgueta <juanelgueta@elguetajj.local>
2021-08-09 20:14:10 -0600

    feature: adds infraestructure for lambda function advertisers.get

--- main.tf
+++ main.tf
@@ -32,3 +32,64 @@ resource "aws_s3_bucket" "lambda_bucket" {
   acl           = "private"
   force_destroy = true
 }
+
+
+data "archive_file" "lambda_advertisers_get" {
+  type = "zip"
+
+  source_dir  = "${path.module}/functions/advertisers-get"
+  output_path = "${path.module}/functions/advertisers-get.zip"
+}
+
+resource "aws_s3_bucket_object" "lambda_advertisers_get" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  key    = "advertisers-get.zip"
+  source = data.archive_file.lambda_advertisers_get.output_path
+
+  etag = filemd5(data.archive_file.lambda_advertisers_get.output_path)
+}
+
+
+resource "aws_lambda_function" "advertisers_get" {
+  function_name = "advertisers-get"
+
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_bucket_object.lambda_advertisers_get.key
+
+  runtime = "nodejs14.x"
+  handler = "index.handler"
+
+  source_code_hash = data.archive_file.lambda_advertisers_get.output_base64sha256
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_cloudwatch_log_group" "advertisers_get" {
+  name = "/aws/lambda/${aws_lambda_function.advertisers_get.function_name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_lambda"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "lambda_bucket_name" {
   value = aws_s3_bucket.lambda_bucket.id
 }
 
+output "function_name" {
+  description = "Name of the Lambda function."
+
+  value = aws_lambda_function.advertisers_get.function_name
+}
