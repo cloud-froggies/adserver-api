9450f5a23c168e81b64efecce90ba70977665518
Juan Elgueta <juanelgueta@elguetajj.local>
2021-08-16 19:03:56 -0600

    agrega modulo apig-gateway

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,24 @@ output "ec2_address" {
 
   value = aws_instance.web.public_dns
 }
+
+
+# Output value definitions
+
+output "lambda_bucket_name" {
+  description = "Name of the S3 bucket used to store function code."
+
+  value = module.lambda_functions.lambda_bucket_name
+}
+
+output "function_name_advertisers_get" {
+  description = "Name of the Lambda function."
+
+  value = module.lambda_functions.function_name_advertisers_get
+}
+
+output "function_name_advertisers_post" {
+  description = "Name of the Lambda function."
+
+  value = module.lambda_functions.function_name_advertisers_post
+}
